6e5582899ba6e7cc858c4d3450a8ea2ea0dcaeca
Janek Bevendorff <janek@jbev.net>
2017-10-25 23:39:42 +0200

    Allow to build other packages for series

--- ubuntu-ppa/build-ppa.sh
+++ ubuntu-ppa/build-ppa.sh
@@ -8,10 +8,11 @@ Build and publish Ubuntu PPA source packages
 
 Options:
   -s, --series        Ubuntu release series to build (required)
+  -p, --package       Package name to build (required, default: 'keepassxc')
   -c, --changelog     CHANGELOG file (required)
   -d, --docker-img    Ubuntu Docker image to use (required)
-  -p, --pkg-version   Package build version (default: 1)
-  -v, --ppa-version   PPA package version (default: 1)
+  -g, --pkg-version   Package build version (default: '0')
+  -v, --ppa-version   PPA package version (default: '1')
   -u, --urgency       Package urgency (default: 'medium')
   -k, --gpg-key       GPG key used to sign package (default: 'BF5A669F2272CF4324C1FDA8CFB4C2166397D0D2')
 EOF
@@ -25,20 +26,22 @@ printError() {
     echo -e "\e[91m${1}\e[0m" >&2
 }
 
+# Usage: runDockerCmd CMD TESTCMD DEPENDENCIES...
 runDockerCmd() {
     local tmpuser="tmpuser${RANDOM}"
     docker run --rm \
             -e "DEBEMAIL=team@keepassxc.org" -e "DEBFULLNAME=KeePassXC Team" \
             -v "$(realpath ..):/debuild:rw" \
             "$DOCKER_IMG" \
-            bash -c "command -v debuild > /dev/null || \
-                     (apt-get --yes update && apt-get --yes --no-install-recommends install ${2}); \
-                     useradd -u $(id -u) ${tmpuser} && cd /debuild/${SERIES} && \
+            bash -c "command -v ${2} > /dev/null || \
+                     (apt-get --yes update && apt-get --yes --no-install-recommends install ${3}); \
+                     useradd -u $(id -u) ${tmpuser} && cd /debuild/${PACKAGE} && \
                      su ${tmpuser} -c '${1}'"
 }
 
-PKG_VERSION="1"
+PKG_VERSION="0"
 PPA_VERSION="1"
+PACKAGE="keepassxc"
 URGENCY="medium"
 GPG_KEY="BF5A669F2272CF4324C1FDA8CFB4C2166397D0D2"
 
@@ -49,6 +52,10 @@ while [ $# -ge 1 ]; do
             SERIES="$2"
             shift ;;
 
+        -p|--package)
+            PACKAGE="$2"
+            shift ;;
+
         -c|--changelog)
             CHANGELOG_FILE="$2"
             shift ;;
@@ -57,7 +64,7 @@ while [ $# -ge 1 ]; do
             DOCKER_IMG="$2"
             shift ;;
 
-        -p|--pkg-version)
+        -g|--pkg-version)
             PKG_VERSION="$2"
             shift;;
 
@@ -81,7 +88,7 @@ while [ $# -ge 1 ]; do
     shift
 done
 
-if [ "$SERIES" == "" ] || [ "$CHANGELOG_FILE" == "" ] || [ "$DOCKER_IMG" == "" ]; then
+if [ "$SERIES" == "" ] || [ "$PACKAGE" == "" ] || [ "$CHANGELOG_FILE" == "" ] || [ "$DOCKER_IMG" == "" ]; then
     printUsage >&2
     exit 1
 fi
@@ -92,10 +99,23 @@ CHANGELOG="$(echo "$FULL_CL" | grep -Pzo "(?s)(?<====\n\n).+" | tr -d '\0' | sed
 DATE="$(date -R)"
 
 
-if [ ! -f "keepassxc_${VERSION}.orig.tar.xz" ]; then
-    printStatus "Downloading sources for version ${VERSION}..."
-    curl -L "https://github.com/keepassxreboot/keepassxc/releases/download/${VERSION}/keepassxc-${VERSION}-src.tar.xz" \
-        > "keepassxc_${VERSION}.orig.tar.xz"
+if [ ! -d "./${SERIES}/${PACKAGE}/debian" ]; then
+    printError "No source folder for package '${PACKAGE}~${SERIES}'!"
+    exit 1
+fi
+
+cd "./${SERIES}/${PACKAGE}"
+
+SOURCE=$(grep -oP "(?<=^keepassxc )https?://.*" ../sources | sed "s/\${VERSION}/${VERSION}/g")
+SOURCE_EXT=$(basename "$SOURCE" | grep -oP "(xz|bz2|gz)\$")
+if [ "" == "${SOURCE}" ]; then
+    printError "No source URL given for package ${PACKAGE}!"
+    cd ../..
+    exit 1
+fi
+if [ ! -f "../${PACKAGE}_${VERSION}.orig.tar.xz" ]; then
+    printStatus "Downloading sources for version ${PACKAGE}-${VERSION}..."
+    curl -L "${SOURCE}" > "../${PACKAGE}_${VERSION}.orig.tar.${SOURCE_EXT}"
     if [ $? -ne 0 ]; then
         printError "Download failed! Process aborted."
         exit 1
@@ -104,35 +124,30 @@ else
     printStatus "Sources already download."
 fi
 
-printStatus "Verifying sources..."
-curl -L "https://github.com/keepassxreboot/keepassxc/releases/download/${VERSION}/keepassxc-${VERSION}-src.tar.xz.sig" | \
-    gpg --verify - "keepassxc_${VERSION}.orig.tar.xz"
+if [ "${PACKAGE}" == "keepassxc" ]; then
+    printStatus "Verifying sources..."
+    curl -L "https://github.com/keepassxreboot/keepassxc/releases/download/${VERSION}/keepassxc-${VERSION}-src.tar.xz.sig" | \
+        gpg --verify - "../${PACKAGE}_${VERSION}.orig.tar.${SOURCE_EXT}"
 
-if [ $? -ne 0 ]; then
-    printError "Signature verification failed! Process aborted."
-    exit 1
+    if [ $? -ne 0 ]; then
+        printError "Signature verification failed! Process aborted."
+        exit 1
+    fi
 fi
 
-if [ ! -d "./${SERIES}/debian" ]; then
-    printError "No source folder for series '${SERIES}'!"
-    cd ..
-    exit 1
-fi
 
 printStatus "Creating source package for '${SERIES}'..."
 
-cd "./${SERIES}"
-
 FULL_VERSION="${VERSION}-${PKG_VERSION}~${SERIES}${PPA_VERSION}"
 
-if $(grep -q "^keepassxc (${FULL_VERSION})" debian/changelog); then
+if $(grep -q "^${PACKAGE} (${FULL_VERSION})" debian/changelog); then
     printError "Changelog entry for '${FULL_VERSION}' already exists!"
     cd ..
     exit 1
 fi
 
 TMP_CL="$(cat debian/changelog)"
-echo -e "keepassxc (${FULL_VERSION}) ${SERIES}; urgency=${URGENCY}\n" > debian/changelog
+echo -e "${PACKAGE} (${FULL_VERSION}) ${SERIES}; urgency=${URGENCY}\n" > debian/changelog
 echo "$CHANGELOG" >> debian/changelog
 echo -e "\n -- KeePassXC Team <team@keepassxc.org>  ${DATE}" >> debian/changelog
 
@@ -141,11 +156,11 @@ if [ "$TMP_CL" != "" ] && [ "$TMP_CL" != $'\n' ]; then
     echo "$TMP_CL" >> debian/changelog
 fi
 
-runDockerCmd "debuild -us -uc -S" "devscripts fakeroot dh-make"
+runDockerCmd "debuild -us -uc -S" "debuild" "devscripts build-essential fakeroot dh-make"
 
 # reproduce what debsign would do, so we can sign on non-Debian based systems
 printStatus "Signing DSC file..."
-DSC_NAME="keepassxc_${FULL_VERSION}.dsc"
+DSC_NAME="${PACKAGE}_${FULL_VERSION}.dsc"
 gpg --clearsign --local-user "${GPG_KEY}" "../${DSC_NAME}"
 rm "../${DSC_NAME}"
 mv "../${DSC_NAME}.asc" "../${DSC_NAME}"
@@ -155,7 +170,7 @@ SHA256UM=$(sha256sum "../${DSC_NAME}" | awk '{ print $1 }')
 SIZE=$(du -b "../${DSC_NAME}" | awk '{ print $1 }')
 
 printStatus "Updating CHANGES file..."
-CHANGES_NAME="keepassxc_${FULL_VERSION}_source.changes"
+CHANGES_NAME="${PACKAGE}_${FULL_VERSION}_source.changes"
 sed -i "s/ [0-f]\\{40\\} [0-9]\+ \(.\+\.dsc\)/ ${SHA1UM} ${SIZE} \1/" "../${CHANGES_NAME}"
 sed -i "s/ [0-f]\\{64\\} [0-9]\+ \(.\+\.dsc\)/ ${SHA256UM} ${SIZE} \1/" "../${CHANGES_NAME}"
 sed -i "s/ [0-f]\\{32\\} [0-9]\+ \(.\+\.dsc\)/ ${MD5SUM} ${SIZE} \1/" "../${CHANGES_NAME}"
@@ -166,6 +181,14 @@ rm "../${CHANGES_NAME}"
 mv "../${CHANGES_NAME}.asc" "../${CHANGES_NAME}"
 
 printStatus "Uploading package..."
-runDockerCmd "cd .. && dput -u ppa:phoerious/keepassxc \"${CHANGES_NAME}\"" "dput"
+runDockerCmd "cd .. && dput -u ppa:phoerious/keepassxc \"${CHANGES_NAME}\"" "dput" "dput"
+
+printStatus "Cleaning up packaging mess..."
+cd ..
+rm "${PACKAGE}_${FULL_VERSION}_source.build"
+rm "${PACKAGE}_${FULL_VERSION}_source.changes"
+rm "${PACKAGE}_${FULL_VERSION}.debian.tar.gz"
+rm "${PACKAGE}_${FULL_VERSION}.dsc"
+rm "${PACKAGE}_${VERSION}.orig.tar.${SOURCE_EXT}"
 
 cd ..
